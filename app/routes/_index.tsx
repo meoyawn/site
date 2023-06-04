13c284d7289caf136db4fa9d166b553966f0ab19
adelnizamutdinov <stiggpwnz@gmail.com>
2023-06-04 19:09:26 +0800

    amen

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
-import { mergeParentMeta } from "../lib/remix"
-import ogimages from "./cv/ogimages.json"
 import { type V2_MetaFunction } from "@remix-run/cloudflare"
 import React, { type JSX } from "react"
+import { mergeParentMeta } from "../lib/remix"
+import ogimages from "./cv/ogimages.json"
 
 export const meta: V2_MetaFunction = mergeParentMeta(() => [
   { title: "Adel Nizamutdinov" },
@@ -76,15 +76,7 @@ const Projects = (): JSX.Element => (
 
 export default function Index(): JSX.Element {
   return (
-    <main className="mx-auto max-w-7xl space-y-8 sm:space-y-12">
-      <section className="space-y-2 px-6 lg:px-8">
-        <h1 className="text-3xl font-extrabold tracking-tight text-gray-800 sm:text-4xl">
-          Adel Nizamutdinov
-        </h1>
-
-        <p className="text-lg">I make software</p>
-      </section>
-
+    <main className="mx-auto max-w-7xl">
       <Projects />
     </main>
   )
